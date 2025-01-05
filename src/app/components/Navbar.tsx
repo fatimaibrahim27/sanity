1f92f95312f21a2742eedefb87c092c9cfde2d3f
fatimaibrahim27 <nazishnazishibrahim257@gmail.com>
2025-01-05 07:56:50 -0800

    Update Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useState } from "react";
 import Link from "next/link";
 import { Twitter, Youtube, Github, Facebook, Linkedin } from "./Socialmedia";  // Make sure this file exists and has the icons defined
